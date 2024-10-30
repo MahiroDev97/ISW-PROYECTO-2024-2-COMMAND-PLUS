0e9d73aa452f85be79e599daaaf7895a08d5476f
Mario Vidal <mariovidal16@gmail.com>
2024-10-29 23:07:24 -0300

    RollBack

---
+++ backend/src/entity/product.entity.js
@@ -0,0 +1,35 @@
+"use strict";
+import { EntitySchema } from "typeorm";
+
+const ProductSchema = new EntitySchema({
+  name: "Product",
+  tableName: "products",
+  columns: {
+    id: {
+      type: "int",
+      primary: true,
+      generated: true,
+    },
+    categoria: {
+      type: "text",
+      nullable: false,
+    },
+    nombre: {
+      type: "text",
+      nullable: false,
+      unique: true,
+    },
+    descripcion: {
+      type: "text",
+      nullable: true,
+    },
+    precio: {
+      type: "int",
+      nullable: false,
+    },
+    diponibilidad: {
+      type: "boolean",
+      default: true,
+    },
+  },
+});
